1fd26f688cac22fd07e560d5774d832f0b06f588
HidakaSe <soqubwhkla748@gmail.com>
2023-07-04 22:55:49 +0900

    Ver1 タンク、フィールド1を作成

---
+++ Assets/Script/Tank/CarController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    Rigidbody rb;
+    float MoveInputValue;
+    float TurnInputValue;
+    public float moveSpeed;
+    public float turnSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Turn();
+    }
+    private void Move()
+    {
+        MoveInputValue = Input.GetAxis("Vertical");
+        Vector3 move = transform.forward * MoveInputValue * moveSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + move);
+    }
+    private void Turn()
+    {
+
+    }
+}
